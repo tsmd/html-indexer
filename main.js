76722847bd231867a9bc0df157c59466a4af666a
Takayuki Shimada <taka@tsmd.jp>
2018-04-11 14:57:36 +0900

    add main.js

---
+++ main.js
@@ -0,0 +1,33 @@
+// src ディレクトリの設定
+const src = 'src/articles'
+
+// base ディレクトリの設定
+const base = 'src'
+
+// files テンプレートと対象ファイル群のマッピング
+const files = {
+  index: {
+    files: '*.hbs'
+  },
+  tags: {
+    files: 'tags/**/*.hbs'
+  }
+}
+
+// files 繰り返す
+
+  // glob
+
+  // 各ファイル 繰り返す
+
+    // タグを取得
+
+    // カテゴリを取得
+
+    // 日付を取得
+
+    // タイトルを取得
+
+    // 概要文を取得
+
+    //
