8c652e4e5e52b95a480e9d99fb7f6fcfd1dfc30b
Takayuki Shimada <taka@tsmd.jp>
2018-04-11 16:53:23 +0900

    タグとか日付とかを取得してくる

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const cheerio = require('cheerio')
 const fs = require('fs')
 const glob = require('glob')
 const path = require('path')
@@ -18,29 +19,45 @@ const files = {
 
 // files 繰り返す
 for (const [name, file] of Object.entries(files)) {
-
   // glob
   glob(path.join('src', file.files), (err, matches) => {
     if (err) {
       throw err
     }
 
+    const entries = []
+
     // 各ファイル 繰り返す
     matches.forEach(match => {
-      const html = fs.readFileSync(match, 'utf-8')
-      console.log(html)
+      const $ = cheerio.load(fs.readFileSync(match, 'utf-8'))
+
+      const data = {}
 
       // タグを取得
+      data.tags = $('meta[name="tag"]').map((i, el) => $(el).attr('content')).get()
 
       // カテゴリを取得
+      data.category = $('meta[name="category"]').attr('content')
 
       // 日付を取得
+      let date = $('meta[name="date"]').attr('content')
+      if (date) {
+        date = new Date(Date.parse(date))
+      }
+      data.date = date
 
       // タイトルを取得
+      data.title = $('title').text()
 
       // 概要文を取得
+      data.description = $('meta[name="description"]').attr('content')
 
+      entries.push({
+        file: match,
+        data
+      })
     })
-  })
 
+    console.log(entries)
+  })
 }
