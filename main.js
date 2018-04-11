bc7da633b192b51055cb9fc08a9b5469e02549a4
Takayuki Shimada <taka@tsmd.jp>
2018-04-12 00:26:44 +0900

    試作一通り

--- main.js
+++ main.js
@@ -1,108 +1,93 @@
 const _ = require('lodash')
 const cheerio = require('cheerio')
 const fs = require('fs')
-const glob = require('glob')
+const glob = require('fast-glob')
 const Handlebars = require('handlebars')
+const mkdirp = require('mkdirp')
 const path = require('path')
 
-// src ディレクトリの設定
-const src = 'src'
-
-// base ディレクトリの設定
-const base = 'src'
+const TagIndexer = require('./lib/tag')
+const CategoryIndexer = require('./lib/category')
+const PageIndexer = require('./lib/page')
+const DateIndexer = require('./lib/date')
+const TitleIndexer = require('./lib/title')
+const DescriptionIndexer = require('./lib/description')
 
 // 設定ファイルを読み込む
 const settings = JSON.parse(fs.readFileSync('settings.json', 'utf-8'))
-settings.tags = new Map(settings.tags)
-console.log(settings.tags)
 
-// files テンプレートと対象ファイル群のマッピング
-const files = {
-  articles: {
-    files: 'articles/**/*.html',
-    template: '*.hbs'
+const forwardSlash = path => path.replace(/\\/g, '/')
+
+const indexers = [
+  new TagIndexer(settings),
+  new CategoryIndexer(settings),
+  new PageIndexer(settings),
+  new DateIndexer(settings),
+  new TitleIndexer(settings),
+  new DescriptionIndexer(settings),
+]
+
+const retrieveEntry = (entryFile) => {
+  const $ = cheerio.load(fs.readFileSync(entryFile, 'utf-8'))
+  const entry = {
+    file: forwardSlash(path.relative(settings.base, entryFile)),
+  }
+  indexers.forEach(plugin => {
+    plugin.retrieveData(entry, $)
+  })
+  return entry
+}
+
+const extractProcessStack = (filePath) => {
+  const matched = filePath.match(/{{.+?}}/g)
+  return matched ? matched.map(tag => tag.slice(2, tag.length - 2)) : []
+}
+
+const processNext = (stack, data, templateFile) => {
+  const process = stack[0]
+  const nextStack = stack.slice(1)
+  if (process) {
+    indexers.forEach(plugin => {
+      if (plugin.exports.includes(process)) {
+        plugin.classify(data, nextContext => processNext(nextStack, nextContext, templateFile))
+      }
+    })
+  } else {
+    render(templateFile, data)
   }
 }
 
-// files 繰り返す
-for (const [name, file] of Object.entries(files)) {
-  // glob
-  glob(path.join(src, file.files), (err, matches) => {
-    if (err) {
-      throw err
-    }
-
-    const entries = []
-
-    // 各ファイル 繰り返す
-    matches.forEach(match => {
-      const $ = cheerio.load(fs.readFileSync(match, 'utf-8'))
-
-      const data = {}
-
-      // タグを取得
-      data.tags = $('meta[name="tag"]').map((i, el) => $(el).attr('content')).get()
-
-      // カテゴリを取得
-      data.category = $('meta[name="category"]').attr('content')
-
-      // 日付を取得
-      let date = $('meta[name="date"]').attr('content')
-      if (date) {
-        date = new Date(Date.parse(date))
-      }
-      data.date = date
-
-      // タイトルを取得
-      data.title = $('title').text()
-
-      // 概要文を取得
-      data.description = $('meta[name="description"]').attr('content')
-
-      entries.push({
-        file: match,
-        data
-      })
-    })
-
-    // インデックス作成の元データを作る
-
-    // タグ別インデックス
-    const tagsIndex = _.chain(entries)
-      .map(entry => entry.data.tags)
-      .flatten()
-      .uniq()
-      .sortBy(tag => [...settings.tags].findIndex(entry => entry[0] === tag))
-      .map(tag => {
-        return {
-          tag: tag,
-          'tag-url': settings.tags.has(tag) ? settings.tags.get(tag).url : tag,
-          entries: entries.filter(entry => entry.data.tags.includes(tag))
-        }
-      })
-      .value()
-
-    // カテゴリ別インデックス
-    // TODO
-
-    // 月別インデックス
-    // TODO
-
-    // ページネーションの処理
-    // TODO
-
-    // インデックスファイルを生成する
-    glob(path.join(src, file.template), (err, matches) => {
-      const processedFiles = []
-
-      matches.forEach(match => {
-        const templateFile = path.join(src, match)
-        const outputFile = path.basename(match, '.hbs') + '.html'
-        const template = Handlebars.compile(fs.readFileSync(match, 'utf-8'))
-        console.log(template({
-          tags: tagsIndex
-        }))
-      })
-    })
-  })
+const processedFiles = []
+const render = (file, data) => {
+  const translatedFile = forwardSlash(path.join(settings.src, path.relative(settings.templateSrc, file)))
+  const baseFilename = Handlebars.compile(translatedFile)(data)
+  const outputFile = path.dirname(baseFilename) + '/' + path.basename(baseFilename, '.hbs') + '.html'
+  if (!processedFiles.includes(outputFile)) {
+    processedFiles.push(outputFile)
+    const template = Handlebars.compile(fs.readFileSync(file, 'utf-8'))
+    mkdirp.sync(path.dirname(outputFile))
+    fs.writeFileSync(outputFile, template(data))
+  }
 }
+
+Object.values(settings.files).forEach(file => {
+  const entryFiles = glob.sync(path.join(settings.src, file.entries))
+  const templateFiles = glob.sync(path.join(settings.templateSrc, file.templates))
+
+  const entries = entryFiles.map(retrieveEntry)
+
+  const context = {
+    entries,
+    contextEntries: _.cloneDeep(entries)
+  }
+
+  indexers.forEach(plugin => {
+    plugin.addIndex(context)
+  })
+
+  templateFiles.forEach(templateFile => {
+    const processStack = extractProcessStack(templateFile)
+    const clonedContext = _.cloneDeep(context)
+    processNext(processStack, clonedContext, templateFile)
+  })
+})
